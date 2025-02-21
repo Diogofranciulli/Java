45a3690cd27f87cca740bd3b284bcce00c83a231
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 22:30:15 -0300

    exercicios

---
+++ WideningCasting.java
@@ -0,0 +1,7 @@
+public class WideningCasting {
+    public static void main(String[] args) {
+        byte valorByte = 18;
+        double valorDouble = valorByte;
+        System.out.println();
+    }
+}
--- exercicios.java
+++ exercicios.java
@@ -1,3 +1,12 @@
-public class exercicios {
+import java.util.Scanner;
 
+public class exercicios {
+    public static void main(String[] args) {
+        int x = 2;
+        String nome = "chiquinho";
+        double valor = 1920.10;
+        boolean amoJava = false;
+        float asndf = 1.5f;
+
+    }
 }
